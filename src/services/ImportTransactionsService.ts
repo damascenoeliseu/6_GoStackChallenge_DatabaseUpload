f268a2a9573fc073553ddec4e38eed955cc40ae8
Eliseu Damasceno <eliseudamasceno@gmail.com>
2020-12-12 00:52:06 -0300

    Implemented test spec: should be able to import transactions

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,9 +1,93 @@
-// import Transaction from '../models/Transaction';
+import { getCustomRepository, getRepository, In } from 'typeorm';
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
+interface CSVTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 class ImportTransactionsService {
-  // async execute(): Promise<Transaction[]> {
-  //   // TODO
-  // }
+  async execute(filePath: string): Promise<Transaction[]> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    const contactsReadStream = fs.createReadStream(filePath);
+
+    const parsers = csvParse({
+      from_line: 2,
+    });
+
+    const parseCSV = contactsReadStream.pipe(parsers);
+    const transactions: CSVTransaction[] = [];
+    const categories: string[] = [];
+
+    parseCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !type || !value) return;
+
+      categories.push(category);
+      transactions.push({ title, type, value, category });
+    });
+
+    await new Promise(resolve => parseCSV.on('end', resolve));
+
+    // Verifica se a categoria que está no CSV existe no banco de dados.
+    // Caso exsta, é armazenada no array existentCategories
+    const existentCategories = await categoriesRepository.find({
+      where: { title: In(categories) },
+    });
+
+    // Desestrutura o array existentCategories para que tenha apenas
+    // a propriedade title
+    const existentCategoriesTitles = existentCategories.map(
+      (category: Category) => category.title,
+    );
+
+    // Cria o array addCategoryTitles para armazenar as categorias que
+    // não existem no banco de dados
+    const addCategoryTitles = categories
+      .filter(category => !existentCategoriesTitles.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    // Cria o array de objetos newCategories com todas as categorias
+    // que devem ser cadastradas no banco de dados
+    const newCategories = categoriesRepository.create(
+      addCategoryTitles.map(title => ({
+        title,
+      })),
+    );
+
+    // Salva no banco de dados
+    await categoriesRepository.save(newCategories);
+
+    // Junta todas as categorias no array finalCategories
+    const finalCategories = [...newCategories, ...existentCategories];
+
+    const createdTransactions = transactionsRepository.create(
+      transactions.map(transaction => ({
+        title: transaction.title,
+        type: transaction.type,
+        value: transaction.value,
+        category: finalCategories.find(
+          category => category.title === transaction.category,
+        ),
+      })),
+    );
+
+    await transactionsRepository.save(createdTransactions);
+
+    await fs.promises.unlink(filePath);
+
+    return createdTransactions;
+  }
 }
 
 export default ImportTransactionsService;
